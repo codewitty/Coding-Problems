278c07de0471de74457f08c143885cefa038c9b4
Joshua G <gomesjoshua@gmail.com>
2020-03-27 19:50:06 -0700

    Create 2_sum_sorted.cpp
    167. Two Sum II - Input array is sorted

---
+++ 2_sum_sorted.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> answer;
+        int i = 0;
+        int j = nums.size()-1;
+        while (i < j) {
+            if (nums[i] + nums[j] < target){
+                i++;
+            }
+            else if (nums[i] + nums[j] > target){
+                j--;
+            }
+            else {
+                answer.push_back(i+1);
+                answer.push_back(j+1);
+                break;
+            }             
+        }
+        return answer;
+    }
+};
