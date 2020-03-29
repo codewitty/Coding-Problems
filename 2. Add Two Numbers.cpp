16766bee6179cfae33506bbc34008f00192ac9ab
Joshua G <gomesjoshua@gmail.com>
2020-03-29 04:16:07 -0700

    Update 2. Add Two Numbers.cpp

--- 2. Add Two Numbers.cpp
+++ 2. Add Two Numbers.cpp
@@ -10,21 +10,17 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int carry = 0;
-        ListNode* res(0);
-        int data = l1->val + l2->val;
-        while(l1 || l2) {
-            if (carry == 0)
-              res->val = data;
-            else {
-                res->val = l1->val + l2->val + 1;
-                carry--;
-            }
-            if (data > 9) 
-                carry = 1;
-            l1 = l1->next;
-            l2 = l2->next;
+        ListNode ans(0);
+        ListNode *res = &ans;
+        
+        while(l1 || l2 || carry) {
+            int data = (l1?l1->val:0) + (l2?l2->val:0) + carry;
+            res->next = new ListNode(data%10);
+            carry = data/10;
+            l1 = l1?l1->next:l1;
+            l2 = l2?l2->next:l2;
             res = res->next;
         }
-       return res; 
+       return ans.next; 
     }
 };
