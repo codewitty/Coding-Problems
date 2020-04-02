ded43271f35c3e7a21d8d0b74b5a02ffc8d59cb3
Joshua G <gomesjoshua@gmail.com>
2020-04-01 23:35:46 -0700

    Update Single Number.cpp

--- 30-day-leetcoding-challenge/Single Number.cpp
+++ 30-day-leetcoding-challenge/Single Number.cpp
@@ -23,3 +23,26 @@ public:
       return retVal;
   }
 };
+
+/*
+Optimal Solution:
+
+using namespace std;
+6	
+7	int singleNumber(vector<int>& nums) {
+8	        int res = nums[0];
+9	        int n;
+10	        for (int i=1;i<nums.size();i++){
+11	            int n = nums[i];
+12	            res = res ^ n;
+13	        }
+14	        return res;
+15	  }
+16	
+17	  
+18	int main() {
+19	    vector<int> v{ 2,5,7,3,2,5,3 };
+20	    cout << "Answer: " << singleNumber(v) << endl;
+21	    return 0;
+22	  }
+*/
