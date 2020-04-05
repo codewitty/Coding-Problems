51fa624eedbaf6bcf8333179e33416527ade5619
Joshua G <gomesjoshua@gmail.com>
2020-04-05 16:24:00 -0700

    Update Move Zeroes.cpp

--- 30-day-leetcoding-challenge/Move Zeroes.cpp
+++ 30-day-leetcoding-challenge/Move Zeroes.cpp
@@ -40,3 +40,27 @@ public:
         }
     }
 };
+
+//Optimized solution from geeksforgeeks
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+   int count = 0;  // Count of non-zero elements 
+  
+    // Traverse the array. If element encountered is non- 
+    // zero, then replace the element at index 'count'  
+    // with this element 
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] != 0) 
+            nums[count++] = nums[i]; // here count is  
+                                   // incremented
+    }
+  
+    // Now all non-zero elements have been shifted to  
+    // front and  'count' is set as index of first 0.  
+    // Make all elements 0 from count to end. 
+    while (count < nums.size()) 
+        nums[count++] = 0; 
+    }
+};
