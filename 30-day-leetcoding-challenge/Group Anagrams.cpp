66b802750af715739958c0d86024bc6bc1a8e32d
Joshua G <gomesjoshua@gmail.com>
2020-04-06 20:18:46 -0700

    Update Group Anagrams.cpp

--- 30-day-leetcoding-challenge/Group Anagrams.cpp
+++ 30-day-leetcoding-challenge/Group Anagrams.cpp
@@ -44,3 +44,27 @@ public:
        return outPut; 
     }
 };
+
+//Accepted submission
+
+class Solution {
+public:
+        vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> result;
+        map<string, vector<string> > checkMap;
+        for (int i=0;i<strs.size();++i){
+            string key = strs[i];
+            sort(key.begin(), key.end());
+            if (checkMap.find(key) == checkMap.end()){
+                checkMap[key] = vector<string>(1,strs[i]);
+            }else{
+               checkMap[key].push_back(strs[i]);
+            }
+        }
+        
+        for (auto it = checkMap.begin(); it !=checkMap.end(); it++){
+            result.push_back(it->second);
+        }
+        return result;
+        
+
