b062df9853669a7c905a7fde0d4b4831e71f9925
Joshua G <gomesjoshua@gmail.com>
2020-04-09 11:27:17 -0700

    Update Backspace String Compare.cpp

--- 30-day-leetcoding-challenge/Backspace String Compare.cpp
+++ 30-day-leetcoding-challenge/Backspace String Compare.cpp
@@ -8,36 +8,36 @@
 using namespace std;
 
 bool backspaceCompare(string S, string T) {
-		// Declare stacks for string processing
+	// Declare stacks for string processing
         stack <char> s;
         stack <char> t;
-		// Declare strings to hold processed input
+	// Declare strings to hold processed input
         string ans1, ans2;
         
-		// Loop that process first string input
+	// Loop that process first string input
         for(char const &c: S){
-			// Check the string character and ensure the string is not empty
+	// Check the string character and ensure the string is not empty
             if (c == '#' && !s.empty())
                 s.pop();
 			// Add character to stack
             else if (c != '#')
                 s.push(c);
         }
-		// Add characters from stack to answer string
+	// Add characters from stack to answer string
         while(!s.empty()){
             ans1.push_back(s.top());
             s.pop();
         }
-		// Loop that process first string input
+	// Loop that process first string input
         for(char const &d: T){
-			// Check the string character and ensure the string is not empty
+	// Check the string character and ensure the string is not empty
             if (d == '#' && !t.empty())
                 t.pop();
 			// Add character to stack
             else if (d != '#')
                 t.push(d);
         }
-		// Add characters from stack to answer string
+	// Add characters from stack to answer string
         while(!t.empty()){
             ans2.push_back(t.top());
             t.pop();
