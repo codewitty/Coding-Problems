896fc7141d9a3dfaa900b3a95c661998314055a8
Joshua G <gomesjoshua@gmail.com>
2020-04-16 19:50:54 -0700

    Update Valid Parenthesis String.cpp

--- 30-day-leetcoding-challenge/Valid Parenthesis String.cpp
+++ 30-day-leetcoding-challenge/Valid Parenthesis String.cpp
@@ -1,3 +1,5 @@
+// Accepted solution
+
 class Solution {
 public:
     bool checkValidString(string s) {
@@ -31,3 +33,39 @@ public:
         
     }
 };
+
+
+// Attempt with stack
+   /*
+    bool checkValidString(string s) {
+        stack<char> stacks;
+        int pCount = 0;
+        int aCount = 0;
+        while(!s.empty()) {
+            if (s[0] == '(' || s[0] == '*') {
+                if (s[0] == '(')
+                    pCount += 1;
+                if (s[0] == '*')
+                    aCount += 1;
+                stacks.push(s[0]);
+                s = s.substr(1, s.size() - 1);
+            }
+            else if(s[0] == ')' && stacks.top() == '('){
+                if (s[0] == ')')
+                    pCount--;
+                stacks.pop();
+                s = s.substr(1, s.size() - 1);
+            }
+            else {
+                s = s.substr(1, s.size() - 1);
+                pCount--;
+            }
+            
+        }
+        while(!stacks.empty() && (aCount != 0 || pCount != 0)){
+            if (stacks.top() == '(')
+                aCount--;
+            stacks.pop();
+        }
+        return (stacks.empty());
+        */
