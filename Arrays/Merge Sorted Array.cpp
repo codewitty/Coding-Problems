183691c8740089b7abdbed37c4e136de699c0f29
Joshua G <gomesjoshua@gmail.com>
2020-05-19 13:37:59 -0700

    Update Merge Sorted Array.cpp

--- Arrays/Merge Sorted Array.cpp
+++ Arrays/Merge Sorted Array.cpp
@@ -12,3 +12,21 @@ public:
         sort(nums1.begin(), nums1.end());
     }
 };
+
+//Driver code for testing
+/*
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main() {
+  vector<int> nums1 = {-2,-1,1,2,3,5,7,9,8,11,0,0,0};
+  vector<int> nums2 = {2, 5, 6};
+  merge(nums1, 10, nums2, 3);
+  
+  for (int i = 0; i < nums1.size(); ++i) {
+    cout << nums1[i] << " " ;
+  }
+  return 0;
+}
+*/
