fca7af6395223dd9ed50254d1c1c5ba669f39b30
Joshua Gomes <gomesjoshua@gmail.com>
2020-06-02 10:27:14 -0400

    added refined solution

--- Arrays/Replace_Elements_with_Greatest_Element_on_Right_Side.cpp
+++ Arrays/Replace_Elements_with_Greatest_Element_on_Right_Side.cpp
@@ -1,3 +1,4 @@
+// First solution O(n^2)
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
@@ -11,5 +12,26 @@ public:
         }
         arr[arr.size() - 1] = -1;
         return arr; 
+    }
+};
+
+// Refined solution O(n)
+
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        int max = arr[arr.size()-1];
+
+        // Set last element to -1
+        arr[arr.size()-1] = -1;
+
+        for(int i = arr.size()-2; i >= 0; --i){
+            int temp = arr[i];
+            arr[i] = max;
+            if (max < temp){
+                max = temp;
+            }
+        }
+        return arr;
     }
 };
