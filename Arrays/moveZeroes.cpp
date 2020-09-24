f5c89ca9aac5587839436659df756fe3cebc0cc8
Joshua Gomes <gomesjoshua@gmail.com>
2020-09-23 21:51:07 -0700

    new function

--- Arrays/moveZeroes.cpp
+++ Arrays/moveZeroes.cpp
@@ -4,30 +4,24 @@
 using namespace std;
 
 void moveZeroes(vector<int>& nums) {
-    int j = 1;
+    int j = 0;
     int temp = 0;
-    for (int i = 0; i < nums.size()-1; i++){
-        while(j < nums.size()){
-            if (nums[i] == 0 && nums[j] != 0){
-             temp = nums[i];
-             nums[i] = nums[j];
-             nums[j] = temp;
-            }
-            j++;
-            if(i != 0)
-                i++;
+    for (int i = 1; i < nums.size(); i++){
+        if (nums[j] == 0 && nums[i] != 0){
+         temp = nums[i];
+         nums[i] = nums[j];
+         nums[j] = temp;
         }
+        if(nums[j] != 0)
+            j++;
     }
 }
 
 int main() {
-vector<int> nums = {0, 1, 0, 1, 3, 4};
-  // nums is passed in by reference. (i.e., without making a copy)
+vector<int> nums = {0, 1, 0, 1, 0, 34, 56, 0, 783, 4};
 
 moveZeroes(nums);
 
-// any modification to nums in your function would be known by the caller.
-// using the length returned by your function, it prints the first len elements.
 for (int i = 0; i < nums.size(); i++) {
     cout << nums[i];
 }
