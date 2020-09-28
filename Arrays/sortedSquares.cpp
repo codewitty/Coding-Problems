23ef58ffc5b0a972f811abcc233b914e548b4cc5
Joshua Gomes <gomesjoshua@gmail.com>
2020-09-27 20:36:52 -0700

    first draft

--- Arrays/sortedSquares.cpp
+++ Arrays/sortedSquares.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <cmath>
 #include <vector>
 
 using namespace std;
 
-vector<int> sortArrayByParity(vector<int>& nums) {
+vector<int> sortedSquares(vector<int>& nums) {
+    for (int i = 0; i < nums.size(); i++){
+        nums[i] = pow(nums[i], 2);
+    }
+
     int j = 0;
     int temp = 0;
     for (int i = 1; i < nums.size(); i++){
@@ -22,13 +27,13 @@ vector<int> sortArrayByParity(vector<int>& nums) {
 // Driver Code
 int main() {
 //vector<int> nums = {0, 0, 1, 1, 5, 6, 8, 5, 9, 67, 3, 1, 2, 4};
-vector<int> nums = {0, 1, 2, 4};
+vector<int> nums = {-4, -1, 0, 1, 3, 10};
 
 cout << "Original Array: ";
 for (int i = 0; i < nums.size(); i++) {
     cout << nums[i] << " ";
 }
-nums = sortArrayByParity(nums);
+nums = sortedSquares(nums);
 
 cout << "\nModified Array: ";
 for (int i = 0; i < nums.size(); i++) {
