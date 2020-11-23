675bcbf0e778f9604295d451891aa63fd8299a75
Joshua Gomes <gomesjoshua@gmail.com>
2020-11-22 23:06:38 -0800

    issues

--- Arrays/heightChecker.py
+++ Arrays/heightChecker.py
@@ -6,18 +6,25 @@ def heightChecker(heights):
     out_sort = [i for i in heights]
     out_sort.sort()
 
-    count = 1
-    i = 1
+    count = 0
+    i = 0
 
     if heights == out_sort:
         count = 0
         return count
 
-    while i < len(heights):
-        if heights[i] < heights[i-1]:
-            heights[i-1], heights[i] = heights[i], heights[i-1]
-            count+=1
+    while i < len(heights) - 1:
+        smol = min(heights)
+        if heights[i] == smol:
+            i+=1
         else:
+            smallest = i
+            while i < len(heights) - 1:
+                if heights[i] > heights[i+1]:
+                    smallest = i+1
+                    i+=1
+            heights[smallest], heights[i] = heights[i], heights[smallest]
+            count+=1
             i+=1
 
     return count
@@ -26,5 +33,5 @@ def heightChecker(heights):
 l1 = [1, 1, 4, 2, 1, 3]
 l2 = [1, 2, 3, 4, 5, 6]
 
-print(heightChecker(l2))
+print(heightChecker(l1))
 
