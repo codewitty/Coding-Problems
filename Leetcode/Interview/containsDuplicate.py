0fb151dd03f5b1fe4569d6588b96c8b6788c876e
Joshua Gomes <gomesjoshua@gmail.com>
2020-12-29 23:52:44 -0800

    new start

---
+++ Leetcode/Interview/containsDuplicate.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+
+        res = []
+        flag = False
+        for num in nums:
+            if num in res:
+                flag = True
+                continue
+            else:
+                res.append(num)
+
+        return flag
+           """
+ 
+        if len(nums) == 0:
+            return False
+        mp = {}
+        for num in nums:
+            if not mp.get(num):
+                mp[num] = True
+            else:
+                return True
+        return False
