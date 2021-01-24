1a84dd032826c3a36ca44f44a3bde91a671e491b
Joshua Gomes <gomesjoshua@gmail.com>
2021-01-23 21:04:06 -0800

    test using assert

--- Leetcode/Interview/Array/twoSum.py
+++ Leetcode/Interview/Array/twoSum.py
@@ -9,9 +9,23 @@ class Solution:
                     output.append(i)
                     output.append(j)
                     return output 
+        return None
 
-obj1 = Solution()
+
+# Test using assert
+
+obj = Solution()
+
+assert obj.twoSum([1, 2, 3], 4) == [0, 2]
+assert obj.twoSum([1234, 5678, 9012], 14690) == [1, 2]
+assert obj.twoSum([2, 2, 3], 4) in [[0, 1], [1, 0]]
+assert obj.twoSum([2, 2], 4) in [[0, 1], [1, 0]]
+assert obj.twoSum([8, 7, 2, 5, 3, 1], 10) in [[0, 2], [2, 0], [1, 4], [4, 1]]
+
+
+"""
 
 arr = [0,0,1,3,12,0]
 
-print(obj1.twoSum(arr, 15))
+print(obj1.obj.twoSum(arr, 15))
+"""
