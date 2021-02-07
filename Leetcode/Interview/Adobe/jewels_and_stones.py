fcf8feb7c1ea65377458f3bef886e6aa1bdc9aa7
Joshua Gomes <gomesjoshua@gmail.com>
2021-02-06 23:51:20 -0800

    Success helper function for asserts

--- Leetcode/Interview/Adobe/jewels_and_stones.py
+++ Leetcode/Interview/Adobe/jewels_and_stones.py
@@ -6,10 +6,15 @@ class Solution:
                 result += 1
         return result
 
+# Helper function for assert success
+
+def myfunc(msg='assert OK'):
+    print (msg)
+    return True
 
 # Test using assert
 obj = Solution()
 
-assert obj.numJewelsInStones("aA", "aAAbbbb") == 3 
-assert obj.numJewelsInStones("aAb", "aAAbbbb") == 7 
-assert obj.numJewelsInStones("aAbC", "acCAAbbbb") == 8 
+assert obj.numJewelsInStones("aA", "aAAbbbb") == 3  and myfunc("Test 1 Success")
+assert obj.numJewelsInStones("aAb", "aAAbbbb") == 7 and myfunc("Test 2 Success")
+assert obj.numJewelsInStones("aAbC", "acCAAbbbb") == 8 and myfunc("Test 3 Success")
