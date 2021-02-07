78be7f651c09d22c7aba08d4717e7667edb4df99
Joshua Gomes <gomesjoshua@gmail.com>
2021-02-06 23:47:09 -0800

    test using assert

--- Leetcode/Interview/Adobe/jewels_and_stones.py
+++ Leetcode/Interview/Adobe/jewels_and_stones.py
@@ -5,3 +5,11 @@ class Solution:
             if stone in jewels:
                 result += 1
         return result
+
+
+# Test using assert
+obj = Solution()
+
+assert obj.numJewelsInStones("aA", "aAAbbbb") == 3 
+assert obj.numJewelsInStones("aAb", "aAAbbbb") == 7 
+assert obj.numJewelsInStones("aAbC", "acCAAbbbb") == 8 
