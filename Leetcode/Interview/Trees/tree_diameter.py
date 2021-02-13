1bb12489cdaba2f7b786fe1032910c1d441b2720
Joshua Gomes <gomesjoshua@gmail.com>
2021-02-12 22:39:27 -0800

    added value as a list to dictionary in this problem

---
+++ Leetcode/Interview/Trees/tree_diameter.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def tree_diameter(self, edges):
+        #Create dictionary
+        tree = {}
+        for edge in edges:
+            if edge[0] in tree.keys():
+                if not isinstance(tree[edge[0]], list):
+                    tree[edge[0]] = [tree[edge[0]]]
+                tree[edge[0]].append(edge[1])
+            else:
+                tree[edge[0]] = edge[1]
+
+        for edge in edges:
+            if edge[1] in tree.keys():
+                if not isinstance(tree[edge[1]], list):
+                    tree[edge[1]] = [tree[edge[1]]]
+                tree[edge[1]].append(edge[0])
+            else:
+                tree[edge[0]] = edge[1]
+                
+        return tree
+
+obj1 = Solution()
+
+edge = [[0,1],[1,2],[2,3],[1,4],[4,5]]
+
+print(obj1.tree_diameter(edge))
