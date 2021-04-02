57674d9a81ba6ff25d27d2bcfddb69173e7a50f9
Joshua Gomes <gomesjoshua@gmail.com>
2021-04-02 09:01:30 -0700

    final solution with main

--- Leetcode/Random/reverse_integer.py
+++ Leetcode/Random/reverse_integer.py
@@ -1,16 +1,48 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        flag = False
-        if x < 1:
-            x *= -1
-            flag = True
-        res = [int(y) for y in str(x)]
-        res = res[::-1]
-        strings = [str(integer) for integer in res]
-        a_string = "".join(strings)
-        result_integer = int(a_string)
-        if flag:
-            result_integer *= -1
-        if result_integer > 2147483647 or result_integer < -2147483647:
+        if -10 < x < 10:
+            return x
+        res = str(abs(x))
+        result = int(res[::-1])
+        if x < 0:
+            result *= -1
+        if result > 2147483647 or result < -2147483647:
             return 0
-        return result_integer
+        return result
+
+def main():
+    obj = Solution()
+    print(f'{obj.reverse(-123)}')
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def reverse(self, x: int) -> int:
+#         flag = False
+#         if x < 1:
+#             x *= -1
+#             flag = True
+#         res = [int(y) for y in str(x)]
+#         res = res[::-1]
+#         strings = [str(integer) for integer in res]
+#         a_string = "".join(strings)
+#         result_integer = int(a_string)
+#         if flag:
+#             result_integer *= -1
+#         if result_integer > 2147483647 or result_integer < -2147483647:
+#             return 0
+#         return result_integer
+
+
+
