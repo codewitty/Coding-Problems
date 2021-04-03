4c0943f918f56c141aa9abaa1c66a0d75be373e3
Joshua Gomes <gomesjoshua@gmail.com>
2021-04-03 16:52:14 -0700

    new problem

---
+++ Leetcode/Random/palindrome_number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return false
+
+
+
+
+
+
+
+def main():
+    obj = Solution()
+    print(f'{obj.reverse(-123)}')
+
+if __name__ == "__main__":
+    main()
