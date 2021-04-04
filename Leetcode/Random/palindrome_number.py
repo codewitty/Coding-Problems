afcb4780d414a4b20017d0ec9639f3055b092b17
Joshua Gomes <gomesjoshua@gmail.com>
2021-04-04 11:29:07 -0700

    Accepted solution

--- Leetcode/Random/palindrome_number.py
+++ Leetcode/Random/palindrome_number.py
@@ -1,17 +1,15 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
-            return false
-
-
-
-
-
-
-
+            return False
+        res = str(x)
+        result = int(res[::-1])
+        if result == x:
+            return True
+    
 def main():
     obj = Solution()
-    print(f'{obj.reverse(-123)}')
+    print(f'{obj.isPalindrome(-123)}')
 
 if __name__ == "__main__":
     main()
