0b978468797fba01f81add97b1252c087d523145
Joshua Gomes <gomesjoshua@gmail.com>
2021-04-05 19:44:50 -0700

    Accepted solution

--- Leetcode/Random/least_common_prefix.py
+++ Leetcode/Random/least_common_prefix.py
@@ -1,21 +1,31 @@
 from typing import List
 
+# class Solution:
+#     def longestCommonPrefix(self, strs: List[str]) -> str:
+#         if len(strs) > 0:
+#             common = [x for x in strs[0]]
+#             strs.pop(0)
+#             for word in strs:
+#                 list_w = [x for x in word]
+#                 ctr = 0
+#                 while ctr < len(list_w) and ctr < len(common) and list_w[ctr] == common[ctr]:
+#                     ctr += 1
+#                 common = common[0:ctr]
+#                 res = "".join(common)
+#         else:
+#             res = ""
+#         return res
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
         if len(strs) > 0:
-            common = [x for x in strs[0]]
-            strs.pop(0)
-            for word in strs:
-                list_w = [x for x in word]
-                ctr = 0
-                while ctr < len(list_w) and ctr < len(common) and list_w[ctr] == common[ctr]:
-                    ctr += 1
-                common = common[0:ctr]
-                res = "".join(common)
-        else:
-            res = ""
+            res = strs[0]
+            for x in range(0, len(strs)):
+                while strs[x].find(res) != 0:
+                    res = res[:len(res)-1]
         return res
-        
+
 def main():
     obj = Solution()
     print(f'{obj.longestCommonPrefix(["flower","flow","flight"])}')
---
+++ Leetcode/Random/valid_parenthesis.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        out = []
+        count = 0
+        for x in s:
+            if x == "(" or x == "[" or x == "{":
+                out.append(x)
+                count += 1
+            elif (x == ")" and count >= 1 and out[count-1] == "("):
+                  del out[count-1]
+                  count -= 1
+            elif (x == "]" and count >= 1 and out[count-1] == "["):
+                  del out[count-1]
+                  count -= 1
+            elif (x == "}" and count >= 1 and out[count-1] == "{"):
+                  del out[count-1]
+                  count -= 1
+            else:
+                return False
+            
+        return not out
