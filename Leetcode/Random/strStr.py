e7ae1f2fa5ce49f94ba55aea9e5a23332d7bdffb
Joshua Gomes <gomesjoshua@gmail.com>
2021-04-05 20:41:29 -0700

    Accepted with tests

---
+++ Leetcode/Random/strStr.py
@@ -0,0 +1,19 @@
+import unittest
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle == "":
+            return 0
+        return haystack.find(needle)
+
+class TestFunction(unittest.TestCase):
+    def test_incorrectInput_single(self):
+        obj = Solution()
+        self.assertEqual(obj.strStr("", ""), 0)
+        self.assertEqual(obj.strStr("aaaaa", "bba"), -1)
+    def test_correctInput_single(self):
+        obj = Solution()
+        self.assertEqual(obj.strStr("hello", "ll"), 2)
+
+if __name__ == "__main__":
+    unittest.main()
