2538fcd6e1ee3285fb4c02795567958a287eb58f
Joshua Gomes <gomesjoshua@gmail.com>
2021-04-13 21:41:42 -0700

    tests added

--- Leetcode/Random/kidsWithCandies.py
+++ Leetcode/Random/kidsWithCandies.py
@@ -1,7 +1,9 @@
+import unittest
+from typing import List
+
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         max_val = max(candies)
-        print(max_val)
         res = []
         for candy in candies:
             if candy + extraCandies >= max_val:
@@ -9,3 +11,14 @@ class Solution:
             else:
                 res.append(False)
         return res
+
+class TestFunction(unittest.TestCase):
+    def test_incorrectInput_single(self):
+        obj = Solution()
+        self.assertEqual(obj.kidsWithCandies([2,3,5,1,3],3), [True,True,True,False,True])
+    def test_correctInput_single(self):
+        obj = Solution()
+        self.assertEqual(obj.kidsWithCandies([12,1,12], 10), [True,False,True])
+
+if __name__ == "__main__":
+    unittest.main()
