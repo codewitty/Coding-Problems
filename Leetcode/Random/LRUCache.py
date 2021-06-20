00343b5db000d93049d074e9c771abeb5b8bc6c4
Joshua Gomes <gomesjoshua@gmail.com>
2021-06-19 19:00:56 -0700

    maybe final

--- Leetcode/Random/LRUCache.py
+++ Leetcode/Random/LRUCache.py
@@ -11,15 +11,22 @@ class LRUCache:
             self.dict2[key] = self.dict2[key] + 1
             print(f'Dict2 after get: {self.dict2}')
             return self.dict[key]
+        else:
+            return -1
 
     def findMin(self) -> int:
-        
-        
-        
+        mins = 2147483647
+        keya = 0
+        for key, value in self.dict2.items():
+            if value < mins:
+                mins = value
+                keya = key
+        del self.dict[keya]
         
 
     def put(self, key: int, value: int) -> None:
         if len(self.dict) == self.capacity:
+            self.findMin()
 
         if key in self.dict:
             self.dict[key] = value
@@ -36,5 +43,4 @@ obj.put(1,1)
 obj.put(2,5)
 param_1 = obj.get(1)
 print(param_1)
-
-
+obj.put(3,7)
