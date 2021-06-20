9b128fe8445f8269e719426e1c7b87b814a16406
Joshua Gomes <gomesjoshua@gmail.com>
2021-06-19 18:54:06 -0700

    new

---
+++ Leetcode/Random/LRUCache.py
@@ -0,0 +1,40 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.dict = {}
+        self.dict2 = {}
+        
+
+    def get(self, key: int) -> int:
+        if key in self.dict:
+            self.dict2[key] = self.dict2[key] + 1
+            print(f'Dict2 after get: {self.dict2}')
+            return self.dict[key]
+
+    def findMin(self) -> int:
+        
+        
+        
+        
+
+    def put(self, key: int, value: int) -> None:
+        if len(self.dict) == self.capacity:
+
+        if key in self.dict:
+            self.dict[key] = value
+        else:
+            self.dict[key] = value
+            self.dict2[key] = 0
+            print(f'Dict1: {self.dict}')
+            print(f'Dict2: {self.dict2}')
+
+
+# Your LRUCache object will be instantiated and called as such:
+obj = LRUCache(2)
+obj.put(1,1)
+obj.put(2,5)
+param_1 = obj.get(1)
+print(param_1)
+
+
--- Leetcode/Random/phoneNumber.py
+++ Leetcode/Random/phoneNumber.py
@@ -6,21 +6,28 @@ class Solution:
     def letterCombinations(self, digits: str) -> str:
         dicts = {"2":["a", "b", "c"], "3":["d", "e", "f"], "4":["g", "h", "i"], "5":["j", "k", "l"], "6":["m", "n", "o"], "7":["p", "q", "r", "s"], "8":["t", "u", "v"], "9":["w", "x", "y", "z"]}
         result = []
-        res = []
-        maxm = 0
+        res = [""]
         for digit in digits:
             if digit in dicts.keys():
                 lists = dicts[digit]
-                maxm = max(maxm, len(lists))
-                for x in lists:
-                    result.append(x)
-        temp =  list(itertools.combinations(result, maxm))
-        for r in temp:
-            res.append(''.join(r))
+                result.append(lists)
+
+        res = result[0]
+        temps = []
+        for i in range(1, len(result)):
+            for l in result[i]:
+                for m in range(0, len(res)):
+                    print(res[m])
+                    temp = str(res[m] + l)
+                    temps.append(temp)
+            print(f'Temps: {temps}')
+            res = temps
+            temps = []
         return res
         
 obj = Solution()
-print(f'Answer: {obj.letterCombinations("27")}')
+print(f'Answer: {obj.letterCombinations("237")}')
+#print(f'Answer: {obj.aggregate([["a", "b", "c"], ["d", "e", "f"]])}')
 
 """
 class TestFunction(unittest.TestCase):
